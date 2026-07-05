210ba4cb7d9dc84231026a3fdbd00b37d8069532
Кристина <kristinakraynova71@gmail.com>
2026-07-05 23:58:52 +0300

    Структура данных о городе

---
+++ app/src/main/java/ru/itis/summerpractice/tudasyudaproject/model/CityData.kt
@@ -0,0 +1,11 @@
+package ru.itis.summerpractice.tudasyudaproject.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CityData (
+    val name: String,
+    val imageUrl: String,
+    val population: Long,
+    val area: Float
+)
