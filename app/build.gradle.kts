c515dcc0f7aedc91f9732305c33f112434e4201f
elvina <elvina18032007@gmail.com>
2026-07-06 20:06:44 +0300

    topola

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,24 +37,3 @@ android {
         compose = true
     }
 }
-
-dependencies {
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.coil)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.activity.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.material)
-    implementation(libs.coil.network.okhttp)
-    implementation(libs.x.navigation)
-    implementation(libs.serialization.json)
-}
