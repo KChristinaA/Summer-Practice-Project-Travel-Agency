fb41449ebe0ca32f303eb81bf8f28f5db2fc3cad
elvina <elvina18032007@gmail.com>
2026-07-08 19:06:59 +0300

    19

--- app/src/main/java/ru/itis/summerpractice/tudasyudaproject/ui/CityScreen.kt
+++ app/src/main/java/ru/itis/summerpractice/tudasyudaproject/ui/CityScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ru.itis.summerpractice.tudasyudaproject.CurrentData
 import ru.itis.summerpractice.tudasyudaproject.model.Route
 import ru.itis.summerpractice.tudasyudaproject.repository.Cities
 import ru.itis.summerpractice.tudasyudaproject.repository.CurrentData
