6a9a021473b2ea0b1670ef0d64ebfe9c227d19c4
Кристина <kristinakraynova71@gmail.com>
2026-07-08 16:18:50 +0300

    Изменена сигнатура класса и способ определения города, на который нажал пользователь

--- app/src/main/java/ru/itis/summerpractice/tudasyudaproject/ui/CityScreen.kt
+++ app/src/main/java/ru/itis/summerpractice/tudasyudaproject/ui/CityScreen.kt
@@ -1,7 +1,11 @@
 package ru.itis.summerpractice.tudasyudaproject.ui
 
-import androidx.navigation.NavController
+import androidx.compose.runtime.Composable
+import ru.itis.summerpractice.tudasyudaproject.CurrentData
+import ru.itis.summerpractice.tudasyudaproject.utils.ConvertCityIndexToCity
 
-fun CityScreen(navController: NavController) {
-    // TODO: брать из статического класса индекс города?
+@Composable
+fun CityScreen(onBackClick: () -> Unit) {
+    val city = ConvertCityIndexToCity(CurrentData.currentSelectedCity)
+
 }
